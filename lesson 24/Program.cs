6ed3b733391e681c42e6ad0e3511ce3efc575619
Student Alexandr <alexandr110282@yandex.ru>
2022-10-03 18:45:22 +0300

    Добавлены новые рабочие папки

---
+++ lesson 24/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine();
+
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+Console.WriteLine(sumDigit);
+Console.WriteLine();
